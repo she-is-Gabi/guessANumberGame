79b15208418811110823bccab5cdcd261e94a596
Gabriella <gabidimitrova1995@gmail.com>
2023-05-04 22:30:25 +0300

    Adding the game code

---
+++ guess-a-number.js
@@ -0,0 +1,38 @@
+function guessANumber() {
+
+    const readline = require('readline').createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    let computerGuess = Math.floor(Math.random() * 100);
+    let guess;
+    let recursiveAsyncReadLine = function () {
+        readline.question(`Guess the number (0-100):`, (number) => {
+
+            guess = Number(number);
+
+            if (guess <= 100 && guess >= 0) {
+
+                if (guess === computerGuess) {
+                    console.log('Ypu guess it!');
+                    return readline.close();
+                } else if (guess < computerGuess) {
+                    console.log('Too low!');
+                    recursiveAsyncReadLine();
+                } else if (guess > computerGuess) {
+                    console.log('Too high');
+                    recursiveAsyncReadLine();
+                } 
+            } else {
+                console.log('Invalid number! Try again.');
+                recursiveAsyncReadLine();
+            }
+
+
+        })
+
+    }
+    recursiveAsyncReadLine()
+}
+guessANumber()
